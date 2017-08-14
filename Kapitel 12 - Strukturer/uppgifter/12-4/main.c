53f9d64985bcc50dd48513be984ef287ab8ade53
Emil Asplund <emilasplund86@gmail.com>
2017-08-14 23:05:22 +0200

    ..

--- Kapitel 12 - Strukturer/uppgifter/12-4/main.c
+++ Kapitel 12 - Strukturer/uppgifter/12-4/main.c
@@ -3,9 +3,38 @@
 struct ratTal{
   int t;
   int n;
-  float faktor;
 };
 
 void shortenTal(struct ratTal *p){
-  
+  int faktor = 1;
+  int madeIt = 0;
+  while(1){
+    if(p->t / faktor <= 1)
+      break;
+    if(p->t % faktor == 0 && p->n % faktor == 0){
+      p->t = p->t / faktor;
+      p->n = p->n / faktor;
+      madeIt++;
+    }
+    faktor++;
+  }
+  if(madeIt == 1)
+    printf("Cannot shorten the number!\n");
+}
+
+void printStruct(struct ratTal *p){
+  printf("%d\n", p->t);
+  printf("-\n");
+  printf("%d\n", p->n);
+}
+
+int main(){
+  struct ratTal tal1;
+
+  tal1.t = 22;
+  tal1.n = 35;
+
+  shortenTal(&tal1);
+  printStruct(&tal1);
+
 }
